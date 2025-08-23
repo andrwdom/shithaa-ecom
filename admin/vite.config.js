d63e53470119a082ec974aa98135aacd5b6e9d12
andrwdom <m.andrewdominic9b@gmail.com>
2025-08-23 07:44:35 +0530

    Last day of work

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,11 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  server: { port: 5174 },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'ui-vendor': ['react-toastify', 'react-hot-toast', 'react-icons'],
+        },
+      },
+    },
+  },
+  server: { 
+    port: 5174,
+    host: true,
+  },
   preview: {
     port: 4173,
     host: '0.0.0.0',
